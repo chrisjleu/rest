e6a6dc4f76d13121313b3f4d0f403f1330fd6e95
Chris <cleung@NCEL4050.iis.amadeus.net>
2014-11-12 15:56:55 +0100

    The code now references a more generically named variable MONGO_URI instead of the vendor specific MONGOLAB_URI.
    - Changing the MongoDb provider is a matter of changing property files and
      system variables anyway, but should this now happen, the name of the
      variable makes more sense. In fact in development it did not make sense
      since the database is running locally and not using a SaaS MongoDb
      provider.

--- core/src/main/java/integration/config/AppConfiguration.java
+++ core/src/main/java/integration/config/AppConfiguration.java
@@ -11,28 +11,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import core.model.Message;
 import core.model.User;
 
 @Configuration
-@ComponentScan(basePackages = { "integration"})
+@ComponentScan(basePackages = { "integration" })
+@PropertySources({ @PropertySource("classpath:default.properties"),
+        @PropertySource(value = "file:/${user.home}/override.properties", ignoreResourceNotFound = true) })
 public class AppConfiguration {
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
+        PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+        pspc.setLocalOverride(true); // Now local (system) properties override property file values
+        return pspc;
     }
 
-    @Value("${MONGOLAB_URI}")
+    @Value("${MONGO_URI}")
     private String mongoDbConnectionUri;
 
     @Bean
     public MongoDbConfiguration mongoDbConfiguration() throws Exception {
         return new MongoDbConfiguration(mongoDbConnectionUri, "mydb", "messages");
     }
-    
+
     // NOTE: Ensure that mongoDbConnectionUri is resolved before this is injected
     @Inject
     MongoFactory mongoFactory;
